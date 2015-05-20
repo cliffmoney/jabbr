0df0253aad6b98553299860b4960369249305464
SteelBurgher <brandon.patrick.ellis@gmail.com>
2015-05-20 14:54:09 -0400

    Ran yeoman generator to make scaffold. Turned on option for socket.io, facebook oAuth.

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('jabbrApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
