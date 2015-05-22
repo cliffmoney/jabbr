6a5a7002a86848dc5acb483410cba4132070a6f5
SteelBurgher <brandon.patrick.ellis@gmail.com>
2015-05-22 17:24:44 -0400

    Added a new messager template and controller. Created a service called Session to store data in a user's session and pass data between controllers.

---
+++ client/app/messager/messager.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('jabbrApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('messager', {
+        url: '/messager',
+        templateUrl: 'app/messager/messager.html',
+        controller: 'MessagerCtrl'
+        authenticate: true
+      });
+  });
