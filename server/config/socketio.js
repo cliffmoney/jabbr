a342b0ae6f28dabba8b97d6cbedc150466806f10
SteelBurgher <brandon.patrick.ellis@gmail.com>
2015-05-22 17:29:39 -0400

    Fixed the socket.io reference error

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -18,7 +18,6 @@ function onConnect(socket) {
   });
 
   // Insert sockets below
-  require('../api/preference/preference.socket').register(socket);
   require('../api/thing/thing.socket').register(socket);
 }
 
