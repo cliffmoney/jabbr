895b78aff84ad48f45ce40878a6f8913c2b5dce1
Jeff Peoples <meisterpeeps@gmail.com>
2015-05-24 15:17:07 -0700

    (FIX) rooms.js merge conflict fixed

--- client/app/dashboard/room/rooms.js
+++ client/app/dashboard/room/rooms.js
@@ -3,21 +3,13 @@ angular.module('jabbrApp')
     $stateProvider
       .state('roomId', {
         url: '/room/:roomId',
-<<<<<<< HEAD:client/app/dashboard/room/rooms.js
         templateUrl: 'app/dashboard/room/views/room.html',
-=======
-        templateUrl: 'app/room/views/room.html',
->>>>>>> (FEAT) Video chat works on localhost; id is given for each room; can join or create rooms.:client/app/room/rooms.js
         controller: 'RoomCtrl'
       })
       .state('room',{
         url: '/room',
-<<<<<<< HEAD:client/app/dashboard/room/rooms.js
         parent: 'dashboard',
         templateUrl: 'app/dashboard/room/views/room.html',
-=======
-        templateUrl: 'app/room/views/room.html',
->>>>>>> (FEAT) Video chat works on localhost; id is given for each room; can join or create rooms.:client/app/room/rooms.js
         controller: 'RoomCtrl'
       });
   });
