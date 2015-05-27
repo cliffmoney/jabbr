23310052919c7aae45005e23d7f43356daf8fdee
SteelBurgher <brandon.patrick.ellis@gmail.com>
2015-05-26 20:56:00 -0400

    Defined the schemas for Partnership and Message, defining relationships

--- server/api/partnership/partnership.model.js
+++ server/api/partnership/partnership.model.js
@@ -4,9 +4,11 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var PartnershipSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  requester: {type: Schema.ObjectId, ref: 'User'},
+  recipient: {type: Schema.ObjectId, ref: 'User'},
+  messages: [{type: Schema.ObjectId, ref: 'Message'}],
+  room_id: {type: String, default undefined},
+  confirmed: {type: Boolean, default: false}
 });
 
 module.exports = mongoose.model('Partnership', PartnershipSchema);
