b29a4ecb374035d4c34ed839244e5b0c078fb0bb
SteelBurgher <brandon.patrick.ellis@gmail.com>
2015-05-26 21:29:07 -0400

    Cleaning up unneeded routes and making the current routes more restful

--- server/api/partnership/index.js
+++ server/api/partnership/index.js
@@ -2,14 +2,15 @@
 
 var express = require('express');
 var controller = require('./partnership.controller');
+var auth = require('../../auth/auth.service');
 
 var router = express.Router();
 
-router.get('/', controller.index);
-router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+router.get('/', auth.isAuthenticated(), controller.index);
+router.get('/:id', auth.isAuthenticated(), controller.show);
+router.post('/', auth.isAuthenticated(), controller.create);
+router.put('/:id', auth.isAuthenticated(), controller.update);
+router.patch('/:id', auth.isAuthenticated(), controller.update);
+router.delete('/:id', auth.isAuthenticated(), controller.destroy);
 
 module.exports = router;
