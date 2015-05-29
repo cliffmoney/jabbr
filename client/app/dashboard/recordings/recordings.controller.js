0cfda01793d8cf2f49a5eafbaf1a3632cd19c430
Jeff Peoples <meisterpeeps@gmail.com>
2015-05-29 15:42:00 -0700

    <FEAT> Users can now listen to their saved recordings.

--- client/app/dashboard/recordings/recordings.controller.js
+++ client/app/dashboard/recordings/recordings.controller.js
@@ -8,19 +8,21 @@ angular.module('jabbrApp')
     $scope.recordingsURL = [];
     var socket = JabbrSocket;
     var currentUser = Auth.getCurrentUser();
-    
-    ss(socket).removeAllListeners("sendRecording"); 
+
+    ss(socket).removeAllListeners("sendRecording");
 
     ss(socket).on("sendRecording", function(recording){
       recording.pipe(blobStream())
       .on('finish', function(){
-        var url = this.toBlobURL();
-        $scope.recordingsURL.push(url);
-        console.log($scope.recordingsURL);
+        $scope.$apply(function(){
+          var url = this.toBlobURL();
+          $scope.recordingsURL.push(url);
+          console.log($scope.recordingsURL);
+        }.bind(this));
        })
     });
 
-    ss(socket).emit("getRecording", currentUser._id);
+    ss(socket).emit("getRecording", currentUser.email);
 
     // ==========
 
