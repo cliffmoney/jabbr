252344b1d9f1f48aac91f712e0d324284179da2f
SteelBurgher <brandon.patrick.ellis@gmail.com>
2015-06-01 18:42:22 -0400

    Set up long polling for new partners in the right dashboard

--- client/components/rightpanel/rightpanel.controller.js
+++ client/components/rightpanel/rightpanel.controller.js
@@ -1,19 +1,37 @@
 'use strict';
 
 angular.module('jabbrApp')
-  .controller('RightPanelCtrl', function ($scope, $location, Auth, User, $http, JabbrSocket, $state) {
-    $scope.socket = JabbrSocket;
+  .controller('RightPanelCtrl', function ($scope, User, $state, $interval) {
 
-    $scope.partners = User.getPartners({id: $scope.currentUser._id});
+    var getPartners = function() {
+      $scope.partners = User.getPartners({id: $scope.currentUser._id});
+    };
 
-
+    getPartners();  // get partners when controller first loads 
+    
     // upon clicking a partner, user gets taken to an overview of the partnership
-
     $scope.seePartnership = function(partnershipId) {
       $state.go('partnership', {partnershipId: partnershipId});
     };
 
-    
+    var promise; // will store the promise in this variable
+    $scope.start = function() {
+      // stops any running interval to avoid two intervals running at the same time
+      $scope.stop(); 
+      // store the interval promise
+      promise = $interval(getPartners, 8000);
+    };
+
+    // stops the interval
+    $scope.stop = function() {
+      $interval.cancel(promise);
+    };
+     
+    $scope.start(); // starts the getting messages interval
+
+    $scope.$on('$destroy', function() {
+      $scope.stop();  // cancels the getting messages interval when user leaves this controller
+    });
     
 
   });
