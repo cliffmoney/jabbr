6f312fbc158e9c139aabf4ddef3d1864ce4f8410
SteelBurgher <brandon.patrick.ellis@gmail.com>
2015-06-05 23:14:41 -0400

    More directive work

--- client/app/dashboard/recordings/recording.directive.js
+++ client/app/dashboard/recordings/recording.directive.js
@@ -1,6 +1,8 @@
 angular.module('jabbrApp').directive('soundButton', [function () {
     return {
         restrict: 'E',
+        template: '<div class="player gradient"><a class="button gradient" ng-click="play()" id="play" href="" title="Play">Play</a><a class="button gradient" id="mute" href="" title=""></a><input type="range" id="seek" value="0" max=""/><a class="button gradient" id="close" href="" title=""></a></div>',
+        replace: false,
         link: function (scope, element, attrs) {
             var audioSrc = attrs.src;
             var audio = new Audio(audioSrc);
@@ -15,14 +17,6 @@ angular.module('jabbrApp').directive('soundButton', [function () {
 
             element.bind('mouseover', function() {
               element.css('cursor', 'pointer');
-            });
-
-            element.css({
-                borderRadius: '50%',
-                width: '100px',
-                height: '100px', 
-                backgroundColor: 'red',
-                display: 'inline-block'
             });
 
         }
