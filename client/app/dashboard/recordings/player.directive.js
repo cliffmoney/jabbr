120bc8a9becbd518bfcf0b758462630d3b7858e2
SteelBurgher <brandon.patrick.ellis@gmail.com>
2015-06-08 13:31:42 -0400

    Implemented a circular audio player with SVG. Next step is making it commentable and giving user ability to select a portion of the track to repeat.

--- client/app/dashboard/recordings/player.directive.js
+++ client/app/dashboard/recordings/player.directive.js
@@ -1,45 +1,97 @@
 angular.module('jabbrApp').directive('circleplay', [function () {
     return {
         restrict: 'E',
-        template: '<svg width="700" height="700" viewbox="0 0 250 250"><path id="border" transform="translate(125, 125)"/><path id="loader" transform="translate(125, 125) scale(.84)"/></svg>',
+        template: '<div class="control-overlay"><h3 class="text-center">With {{recording.partner}} on {{parseDate(recording.date)}}</h3><button id="pButton" class="play center-block" ng-click="play()"><i class="fa fa-play-circle fa-2x"></i></button></div><svg id="svg-player" width="700" height="700" viewbox="0 0 700 700"><circle id="larger-circle" cx="350" cy="350" r="349" /><path id="border" transform="translate(350, 350)"/><circle id="center-circle" cx="350" cy="350" r="325"/></svg>',
         replace: false,
         link: function (scope, element, attrs) {
-            // var audioSrc = attrs.src;
-            // var audio = new Audio(audioSrc);
-            // var playhead = $('#playhead');
-            // var timeline = $('#timeline');
+            var audioSrc = attrs.src;
+            var audio = new Audio(audioSrc);
+            var largerCircle = Snap('#larger-circle');
+            var track = Snap('#border');
             // var commentBox = $('.comment-row');
             // var commentText = $('#commentText');
-            // var duration;
+            var duration;
+            var circleCenterX = 350;
+            var circleCenterY = 350;
+            var play = false;
             // var timelineWidth = timeline.width();
             // var timelineOffset = timeline.offset().left;
-
             var loader = document.getElementById('loader')
               , border = document.getElementById('border')
               , α = 0
-              , π = Math.PI
-              , t = 30;
+              , π = Math.PI;
 
-            (function draw() {
-              α++;
-              α %= 360;
+            largerCircle.click(function(event) {
+              moveTrack(event);
+            });
+
+            track.click(function(event) {
+              moveTrack(event);
+            });
+
+            audio.addEventListener("canplaythrough", function () {
+              duration = audio.duration;
+            }, false);
+
+            var moveTrack = function(e) {
+              var x = (e.offsetX - 350);
+              var y = (e.offsetY - 350);
+              var radius = (Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)));
+              var radians;
+              if(y < 0 && x >= 0) {
+                radians = -Math.atan((x/radius)/(y/radius));
+              } else if (y >= 0 && x >= 0){
+                radians = π - Math.atan((x/radius)/(y/radius));
+              } else if (y >= 0 && x < 0) {
+                radians = π - Math.atan((x/radius)/(y/radius));
+              } else if (y < 0 && x < 0) {
+                radians = 2*π - Math.atan((x/radius)/(y/radius));
+              }
+              audio.currentTime = audio.duration * (radians/(2 * π));
+              if(!play) {
+                draw();
+              }
+            }
+
+            var draw = function() {
+              α = 360 * (parseFloat(audio.currentTime)/audio.duration);
+
               var r = ( α * π / 180 )
-                , x = Math.sin( r ) * 125
-                , y = Math.cos( r ) * - 125
+                , x = Math.sin( r ) * 350
+                , y = Math.cos( r ) * -350
                 , mid = ( α > 180 ) ? 1 : 0
-                , anim = 'M 0 0 v -125 A 125 125 1 ' 
+                , anim = 'M 0 0 v -350 A 350 350 1 ' 
                        + mid + ' 1 ' 
                        +  x  + ' ' 
                        +  y  + ' z';
               //[x,y].forEach(function( d ){
               //  d = Math.round( d * 1e3 ) / 1e3;
               //});
-             
-              loader.setAttribute( 'd', anim );
-              border.setAttribute( 'd', anim );
               
-              setTimeout(draw, t); // Redraw
-            })();
+              
+              border.setAttribute( 'd', anim );
+              if(play)
+                setTimeout(draw, 30);
+            };
+            
+            // audio.addEventListener('timeupdate', function() {
+            //   var playPercent = parseFloat(audio.currentTime) / audio.duration;
+            //   draw(playPercent);
+            // });
+
+            scope.play = function () {
+                if (audio.paused) {
+                    play = true;
+                    draw();
+                    audio.play();
+                } else {
+                    play = false;
+                    audio.pause();
+                }
+            };
+
+
+
 
             // audio.addEventListener('timeupdate', function() {
             //   var playPercent = 100 * (parseInt(audio.currentTime, 10) / audio.duration);
